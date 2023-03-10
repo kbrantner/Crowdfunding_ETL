9459b5db76c49f6efb01d67e18129012de5ca028
kbrantner <brantner.kelsey@gmail.com>
2023-03-09 22:05:08 -0800

    adding completed csv files from python code

---
+++ Starter_Files/Resources/crowdfunding_db_schema_ERD.sql
@@ -0,0 +1,58 @@
+﻿
+
+CREATE TABLE "category" (
+    "cat_ids" varchar   NOT NULL,
+    "categories" varchar   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "cat_ids"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar   NOT NULL,
+    "subcategory" varchar   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "email" varchar   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "description" varchar   NOT NULL,
+    "goal" decimal   NOT NULL,
+    "pledged" decimal   NOT NULL,
+    "outcome" varchar   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar   NOT NULL,
+    "currency" varchar   NOT NULL,
+    "launched_date" varchar   NOT NULL,
+    "end_date" varchar   NOT NULL,
+    "cat_ids" varchar   NOT NULL,
+    "subcategory_id" varchar   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_cat_ids" FOREIGN KEY("cat_ids")
+REFERENCES "category" ("cat_ids");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+
